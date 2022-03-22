c043e9c4ad0b57f4cb61b877d159058571d9168b
martin835 <64438132+martin835@users.noreply.github.com>
2022-03-22 16:50:16 +0100

    Fetching data for blogList on the main page. Route to single blog / article broken

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -2,18 +2,49 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
+import { useState, useEffect } from "react";
 
 const BlogList = (props) => {
+  const [postInfo, setPostInfo] = useState([]);
+
+  useEffect(() => {
+    loadPostInfo();
+  }, []);
+
+  const loadPostInfo = async () => {
+    console.log("i am mounted");
+
+    try {
+      let response = await fetch("http://localhost:3001/blogPosts", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        let data = await response.json();
+        console.log(data);
+
+        setPostInfo(data);
+        //setIsLoading(false);
+      } else {
+        alert("something went wrong :(");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Row>
-      {posts.map((post) => (
+      {postInfo.map((post) => (
         <Col
           md={4}
           style={{
             marginBottom: 50,
           }}
         >
-          <BlogItem key={post.title} {...post} />
+          <BlogItem key={post._id} {...post} />
         </Col>
       ))}
     </Row>
